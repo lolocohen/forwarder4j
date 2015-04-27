fbc5c0b9d23a678614b41e36a43d69d45a9598ab
lcohen <lcohen@lcohen-CSL>
2015-04-27 15:42:57 +0200

    fixed run scripts

--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@
 rem Java 7 or later is required
 rem set JAVA_HOME=C:\java\jdk1.7.0
 rem set PATH=%JAVA_HOME%\bin;%PATH%
-call java -cp config;classes;lib/* org.forward4j.Forwarder
+call java -cp config;Forwarder4j.jar;lib/* org.forwarder4j.Forwarder
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 # Java 7 or later is required
 #export JAVA_HOME=/opt/java/jdk1.7.0
 #export PATH=$JAVA_HOME/bin:$PATH
-java -cp config:classes:lib/* org.forward4j.Forwarder
+java -cp config:Forwarder4j.jar:lib/* org.forwarder4j.Forwarder
