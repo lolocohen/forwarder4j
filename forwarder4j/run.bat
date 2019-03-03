e58795125cc40341906445416fa346eaad9c6d0c
Laurent Cohen <laurent.cohen@jppf.org>
2019-03-03 08:42:54 +0100

    implemented issue #4

--- forwarder4j/run.bat
+++ forwarder4j/run.bat
@@ -1,3 +1,3 @@
 @echo off
 :: Java 8 or later is required
-call java -cp config;lib/* org.forwarder4j.Forwarder %*
+call java -cp config;lib/* -Djava.util.logging.config.file=config/logging.properties org.forwarder4j.Forwarder %*
